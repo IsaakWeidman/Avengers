eb3d62f1c527521e3f649d9e80aee28cab808adc
IsaakWeidman <44002541+IsaakWeidman@users.noreply.github.com>
2018-10-17 16:48:47 -0400

    Avengers now reads in the script and prints it to the screen.
    I used a BufferedReader rather than a Scanner mainly because the Scanner wasn't working properly and because BufferedReaders are more efficient  for large amounts of data.

--- Avengers/src/Avengers.java
+++ Avengers/src/Avengers.java
@@ -1,14 +1,43 @@
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Avengers {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		
+		//Holds the script line by line
+		List<String> script;
+		
+		//Read in the script using the read in method
+		script = readFile(new File("AvengersScript.txt"));
+		
+		//Print the script to the screen
+		for(String s : script) {
+			System.out.println(s);
+		}
+	}//end main()
 
-    }
-
-//======================================================================================================================
-
-    public static void readInFile(File file) {
-        Scanner scan = new Scanner(file);
-    }
-}
+	//===================================================================================================================
+	
+	//Reads in a specified file into an array list and returns the result
+	public static List<String> readFile(File file) {
+		
+		List<String> result = new ArrayList<>();
+		
+		try {
+			
+			String line;
+			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			//Adds line to the result list. if line == null, then the file is completely read in.
+			while ((line = br.readLine()) != null) {
+				result.add(line);
+			}
+			
+		} catch (IOException e) {
+			System.out.printf("File \'%s\' was not found.", file.getName());
+		}
+		
+		return result;
+	}//end readFile()
+}//end Avengers.java
