a67d0bbb3bc3e5574a3f9a2d0952fc57cf4396e0
IsaakWeidman <44002541+IsaakWeidman@users.noreply.github.com>
2018-10-17 14:28:18 -0400

    Started Scanner

--- Avengers/src/Avengers.java
+++ Avengers/src/Avengers.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Avengers {
     public static void main(String[] args) {
-        
+
+    }
+
+//======================================================================================================================
+
+    public static void readInFile(File file) {
+        Scanner scan = new Scanner(file);
     }
 }
